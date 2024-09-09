42a73fc9b20a59693536a18d05771b637ff7a154
Cde571 <cristian.echeverry1@udea.edu.co>
2024-09-09 09:44:39 -0500

    Paso 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import dotenv from 'dotenv';
 
 import tailwind from '@astrojs/tailwind';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 dotenv.config();
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()]
 });
